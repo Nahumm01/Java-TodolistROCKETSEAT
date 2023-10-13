7665479103cfbb6ebedbd889d94d3ccc896b89bc
Thiago Nahum <nahumm01@outlook.com>
2023-10-12 23:20:39 -0300

    add HttpStatus Codes nas condicionais de request

--- src/main/java/br/com/rocketseat/todolist/user/UserController.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserController.java
@@ -3,6 +3,9 @@ package br.com.rocketseat.todolist.user;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,19 +17,19 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
 
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
             System.out.println("Usuário já existente!");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
         } else {
             
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
     }
 }
