ef4a642b7f509c1c63e3d5a118b0de953c1aca98
Thiago Nahum <nahumm01@outlook.com>
2023-10-14 00:06:17 -0300

    criando validação de username e password

--- src/main/java/br/com/rocketseat/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/rocketseat/todolist/filter/FilterTaskAuth.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.util.Base64;
 
 import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.rocketseat.todolist.user.IUserRepository;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -17,6 +20,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
@@ -35,7 +42,22 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         System.out.println(username);
         System.out.println(password);
 
-        filterChain.doFilter(request, response);
+        // Validação de Usuário
+        var user = this.userRepository.findByUsername(username);
+        if (user == null) {
+            response.sendError(401);
+
+        } else {
+
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if (passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+            } else{
+                response.sendError(401);
+            }
+
+        }
+
     }
 
 }
